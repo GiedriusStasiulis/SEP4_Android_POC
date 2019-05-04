d22e3918db585e4c737b5bf3864e292e6e5af5ed
apetriceanu <apetriceanu@live.com>
2019-05-04 19:52:28 +0200

    Create greenhouse select layout; add activity

---
+++ App_v1/app/src/main/java/com/example/app_v1/activities/GreenhouseSelectActivity.java
@@ -0,0 +1,25 @@
+package com.example.app_v1.activities;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import com.example.app_v1.viewmodels.GreenhouseSelectActivityViewModel;
+
+public class GreenhouseSelectActivity extends AppCompatActivity {
+    private static final String TAG = "GreenhouseSelectActivity";
+
+    public Spinner selectGreenhouse;
+    public Button buttonGo;
+
+    private GreenhouseSelectActivityViewModel viewModel;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
---
+++ App_v1/app/src/main/java/com/example/app_v1/viewmodels/GreenhouseSelectActivityViewModel.java
@@ -0,0 +1,6 @@
+package com.example.app_v1.viewmodels;
+
+import android.arch.lifecycle.ViewModel;
+
+public class GreenhouseSelectActivityViewModel extends ViewModel {
+}
