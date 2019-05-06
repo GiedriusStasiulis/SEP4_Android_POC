620da2d4f7ce9405d74f8028e1e72bec86129b15
Daniela Porcescu <danielaporcescu1@gmail.com>
2019-05-06 15:18:12 +0200

    Moved Log_In_Activity to activities folder

--- App_v1/app/src/main/java/com/example/app_v1/activities/LogInActivity.java
+++ App_v1/app/src/main/java/com/example/app_v1/activities/LogInActivity.java
@@ -1,4 +1,4 @@
-package com.example.app_v1;
+package com.example.app_v1.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,6 +11,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.app_v1.MainActivity;
+import com.example.app_v1.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
