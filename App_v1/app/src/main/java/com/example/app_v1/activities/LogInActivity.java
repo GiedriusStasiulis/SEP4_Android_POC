ca04bfaf5910a5bfda604f65fbdaf4bffec144c0
Daniela Porcescu <danielaporcescu1@gmail.com>
2019-05-12 15:48:13 +0200

    Saving last entered email when when pressing "remember email" checkbox

--- App_v1/app/src/main/java/com/example/app_v1/activities/LogInActivity.java
+++ App_v1/app/src/main/java/com/example/app_v1/activities/LogInActivity.java
@@ -1,13 +1,17 @@
 package com.example.app_v1.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -20,11 +24,15 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class LogInActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private SharedPreferences mPreferences;
+    private  SharedPreferences.Editor mEditor;
+
     private FirebaseAuth mAuth;
     private static final String TAG = "EmailPassword";
-    private EditText mEmailField;
-    private EditText mPasswordField;
-    private Button logIn;
+    private EditText mEmail;
+    private EditText mPassword;
+    private CheckBox mCheckBox;
+    private Button buttonLogIn;
 
 
     @Override
@@ -34,18 +42,64 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
         mAuth = FirebaseAuth.getInstance();
 
         // Views
-        mEmailField = findViewById(R.id.email);
-        mPasswordField = findViewById(R.id.password);
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
 
         //Buttons
-        logIn=findViewById(R.id.log_in_button);
-
-        logIn.setOnClickListener(new View.OnClickListener() {
+        buttonLogIn =findViewById(R.id.log_in_button);
+        buttonLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //save the checkbox preference
+                if(mCheckBox.isChecked()){
+                    //set a checkbox when the application starts
+                    mEditor.putString(getString(R.string.checkBox), "True");
+                    mEditor.commit();
+
+                    //save email
+                    String email = mEmail.getText().toString();
+                    mEditor.putString(getString(R.string.email), email);
+                    mEditor.commit();
+                }else{
+                    //set a checkbox when the application starts
+                    mEditor.putString(getString(R.string.checkBox), "False");
+                    mEditor.commit();
+
+                    //save email
+                    mEditor.putString(getString(R.string.email), "");
+                    mEditor.commit();
+
+                }
                 logIn();
             }
         });
+
+        mCheckBox = findViewById(R.id.checkBox);
+
+        //Objects
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor=mPreferences.edit();
+
+        checkSharedPreferences();
+    }
+
+    private void checkSharedPreferences()
+    {
+
+        String checkbox = mPreferences.getString(getString(R.string.checkBox), "false");
+        String email= mPreferences.getString(getString(R.string.email), "");
+
+        mEmail.setText(email);
+
+        if(checkbox.equals("True")){
+            mCheckBox.setChecked(true);
+        }
+        else{
+            mCheckBox.setChecked(false);
+        }
+
+
+
     }
 
     private void updateUI(FirebaseUser user) {
@@ -83,20 +137,20 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
     private boolean validateForm() {
         boolean valid = true;
 
-        String email = mEmailField.getText().toString();
+        String email = mEmail.getText().toString();
         if (TextUtils.isEmpty(email)) {
-            mEmailField.setError("Required.");
+            mEmail.setError("Required.");
             valid = false;
         } else {
-            mEmailField.setError(null);
+            mEmail.setError(null);
         }
 
-        String password = mPasswordField.getText().toString();
+        String password = mPassword.getText().toString();
         if (TextUtils.isEmpty(password)) {
-            mPasswordField.setError("Required.");
+            mPassword.setError("Required.");
             valid = false;
         } else {
-            mPasswordField.setError(null);
+            mPassword.setError(null);
         }
 
         return valid;
@@ -106,13 +160,13 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         int i = v.getId();
         if (i == R.id.log_in_button) {
-            signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
+            signIn(mEmail.getText().toString(), mPassword.getText().toString());
         }
     }
 
     public void logIn()
     {
-        signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
+        signIn(mEmail.getText().toString(), mPassword.getText().toString());
     }
 
     public void goNextPage(){
